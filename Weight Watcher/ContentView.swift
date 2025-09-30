55e3a1e6f1904a492be39e78d1ac00c1ef93b23f
Tjark Saul <mail@appfel.dev>
2025-09-30 11:58:02 +0200

    Initial Commit

---
+++ Weight Watcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weight Watcher
+//
+//  Created by Tjark Saul on 30.09.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weight Watcher/Weight_WatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weight_WatcherApp.swift
+//  Weight Watcher
+//
+//  Created by Tjark Saul on 30.09.25.
+//
+
+import SwiftUI
+
+@main
+struct Weight_WatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
